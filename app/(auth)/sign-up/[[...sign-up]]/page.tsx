bee4582a3d6294f609625cfdada652bfde3d8712
Muhammad Hassaan Shaukat <mr.hassaan50@gmail.com>
2024-08-05 10:03:09 +0500

    Begnning User Flow

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -3,7 +3,7 @@ import { SignUp } from "@clerk/nextjs";
 export default function Page() {
   return (
     <div className="flex items-center justify-center h-full">
-      <SignUp afterSignOutUrl="/" />
+      <SignUp afterSignUpUrl={'/profile'} afterSignOutUrl="/" />
     </div>
   );
 }
