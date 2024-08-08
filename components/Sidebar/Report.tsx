36679eed44e214861cb976c79679125a1517b9a0
Muhammad Hassaan Shaukat <mr.hassaan50@gmail.com>
2024-08-08 08:12:14 +0500

    New commit

--- components/Sidebar/Report.tsx
+++ components/Sidebar/Report.tsx
@@ -4,13 +4,7 @@ import Form from '../Feedback'
 
 const Report = () => {
   return (
-<<<<<<< HEAD
-    <div  className='flex flex-col items-center justify-center'>
-      <Bug /><span>Report</span>
-      </div>
-=======
     <div  className='flex flex-col items-center justify-center'><Form /></div>
->>>>>>> 1c77dbe775ca5de396ddbbceb199ddb505dfd014
   )
 }
 
