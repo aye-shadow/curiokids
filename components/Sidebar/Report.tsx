098ced84ad4b0152f59ab340ca479cd2f6777a38
kcl <kcleck06@gmail.com>
2024-08-08 08:15:55 +0800

    Deleted pages directory, moved Feedback.tsx into components and imported Form into Report.tsx

--- components/Sidebar/Report.tsx
+++ components/Sidebar/Report.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { Bug } from 'lucide-react';
+import Form from '../Feedback'
 
 const Report = () => {
   return (
-    <div  className='flex flex-col items-center justify-center'><Bug /><span>Report</span></div>
+    <div  className='flex flex-col items-center justify-center'><Form /><span>Report</span></div>
   )
 }
 
